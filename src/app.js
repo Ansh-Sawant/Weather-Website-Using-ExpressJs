0d62396a03fcf61736a9fb992e7568ca64bfafcd
Ansh Sawant <sawantansh17@gmail.com>
2021-05-10 13:46:07 +0530

    About & Error404  page completed

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const path = require("path");
+const hbs = require("hbs");
 const app = express();
 const port = process.env.PORT || 8000;
 
 // public static path
 const static_path = path.join(__dirname, "../public");
+const template_path = path.join(__dirname, "../templates/views");
+const partial_path = path.join(__dirname, "../templates/partials");
 
 app.set("view engine", "hbs");
+app.set("views", template_path);
+hbs.registerPartials(partial_path);
 
 app.use(express.static(static_path));
 
@@ -23,7 +28,9 @@ app.get("/weather", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.render("404error");
+  res.render("404error", {
+    errorMsg: "Opps! Page Not Found",
+  });
 });
 
 app.listen(port, () => {
